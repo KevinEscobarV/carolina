1fae14c6f22c6d23566683c3f6d71f14a29039b0
Kevin David Escobar Villegas <escobarkevin567@gmail.com>
2024-03-03 23:50:48 -0500

    Add like

--- database/seeders/ImportTransactions.php
+++ database/seeders/ImportTransactions.php
@@ -35,10 +35,9 @@ class ImportTransactions extends Seeder
                 if ($row['recibo_no']) {
                     if ($row['mz'] && $row['lote']) {
 
+                        $block = Block::where('code', 'like', '%' . strval($row['mz']) . '%')->first();
 
-                        $block = Block::where('code', strval($row['mz']))->first();
-
-                        $parcel = Parcel::where('number', strval($row['lote']))->where('block_id', $block->id)->first();
+                        $parcel = Parcel::where('number', 'like', '%' . strval($row['lote']) . '%')->where('block_id', $block->id)->with('promise')->first();
 
                         if ($parcel) {
 
